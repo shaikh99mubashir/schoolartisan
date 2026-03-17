010a5124517ddf0e04eeefe434a7656ea7d78fb0
shaikh99mubashir <shaikh99mubashir@gmail.com>
2026-03-17 20:48:02 +0500

    final og

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,12 +9,12 @@ export default function Document() {
         <meta name="theme-color" content="#1648F4" />
         <meta property="og:locale" content="en_US" />
         <meta property="og:site_name" content="School Artisan" />
-        <meta property="og:image" content="https://schoolartisan.com/og-image.png" />
+        <meta property="og:image" content="https://schoolartisan.vercel.app/og-image.jpg" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:type" content="image/jpeg" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="https://schoolartisan.com/og-image.png" />
+        <meta name="twitter:image" content="https://schoolartisan.vercel.app/og-image.jpg" />
       </Head>
       <body className="antialiased">
         <Main />
