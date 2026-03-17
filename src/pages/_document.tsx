82f200271eee8c6c402c0ddd3eba53a218976b31
shaikh99mubashir <shaikh99mubashir@gmail.com>
2026-03-17 07:17:48 +0500

    add env and fav icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
